04a6da19f279f4169063b0b9ec71e0ba13524ef0
R Sánchez <rene.as.himself@gmail.com>
2019-01-14 13:34:15 -0600

    Added basic timer functionality

--- src/components/PomodoroClock.js
+++ src/components/PomodoroClock.js
@@ -10,34 +10,23 @@ import upIcon from '../assets/up-arrow.svg'
 import downIcon from '../assets/down-arrow.svg'
 
 const Button = props => (
-        <button
-            id={props.id}
-            className='button'
-            onClick={props.onClick}>
-
-            <img
-                src={props.icon}
-                alt={props.id}
-                className='icon'></img>
-
-        </button>
-    )
+    <button id={props.id} className='button' onClick={props.onClick}>
+        <img src={props.icon} alt={props.id} className='icon' />
+    </button>
+)
 
 Button.propTypes = {
     id: PropTypes.string.isRequired,
 }
 
 const Label = props => (
-        <div
-            id={props.label}
-            className='container__length__label'
-        >
-            <h3>{props.name}</h3>
-            <div className='container__length__label__children'>
-                {props.children}
-            </div>
+    <div id={props.label} className='container__length__label'>
+        <h3>{props.name}</h3>
+        <div className='container__length__label__children'>
+            {props.children}
         </div>
-    )
+    </div>
+)
 
 Label.propTypes = {
     label: PropTypes.string.isRequired,
@@ -52,7 +41,12 @@ class PomodoroClock extends Component {
 
         this.handleReset = this.handleReset.bind(this)
         this.playAudio = this.playAudio.bind(this)
-        this.playPause = this.playPause.bind(this)
+        this.beginCountDown = this.beginCountDown.bind(this)
+        this.decrementTimer = this.decrementTimer.bind(this)
+        this.phaseControl = this.phaseControl.bind(this)
+        this.switchTimer = this.switchTimer.bind(this)
+        this.clockify = this.clockify.bind(this)
+        this.timerControl = this.timerControl.bind(this)
         this.decrementValue = this.decrementValue.bind(this)
         this.incrementValue = this.incrementValue.bind(this)
         this.changeTimer = this.changeTimer.bind(this)
@@ -61,8 +55,10 @@ class PomodoroClock extends Component {
     initialState = {
         breakLength: 5,
         sessionLength: 25,
-        timer: 25,
-        currentTimer: 'Session',
+        timer: 1500, // 25 min
+        timerType: 'Session',
+        timerState: 'stopped',
+        // intervalID: '',
     }
 
     handleReset() {
@@ -70,13 +66,84 @@ class PomodoroClock extends Component {
 
         this.audioBeep.pause()
         this.audioBeep.currentTime = 0
+
+        // if (this.state.intervalID) {
+        // this.state.intervalID.cancel()
+        clearInterval(this.interval)
+        // }
     }
 
     playAudio() {
         this.audioBeep.play()
     }
 
-    playPause() {}
+    timerControl() {
+        // stopped
+        if (this.state.timerState === 'stopped') {
+            this.beginCountDown()
+            this.setState({ timerState: 'running' })
+        }
+        // running
+        else {
+            this.setState({ timerState: 'stopped' })
+            // this.state.intervalID.cancel()
+            clearInterval(this.interval)
+        }
+    }
+
+    beginCountDown() {
+        // setTimeout(() => {
+        this.interval = setInterval(() => {
+            this.decrementTimer()
+            this.phaseControl()
+        }, this.state.timer)
+
+        // this.setState({
+        //     intervalID: INTERVAL,
+        // })
+        // }, this.state.timer)
+    }
+
+    phaseControl() {
+        const timer = this.state.timer
+
+        if (timer < 0) {
+            if (this.state.timerType === 'Session') {
+                // this.state.intervalID.cancel()
+                clearInterval(this.interval)
+                this.beginCountDown()
+                this.switchTimer(this.state.breakLength * 60, 'Break')
+            } else {
+                // this.state.intervalID.cancel()
+                clearInterval(this.interval)
+                this.beginCountDown()
+                this.switchTimer(this.state.sessionLength * 60, 'Session')
+            }
+
+            this.playAudio()
+        }
+    }
+
+    switchTimer(num, str) {
+        this.setState({
+            timer: num,
+            timerType: str,
+        })
+    }
+
+    decrementTimer() {
+        this.setState({ timer: this.state.timer - 1 })
+    }
+
+    clockify() {
+        let minutes = Math.floor(this.state.timer / 60)
+        let seconds = this.state.timer - minutes * 60
+
+        seconds = seconds < 10 ? `0${seconds}` : seconds
+        minutes = minutes < 10 ? `0${minutes}` : minutes
+
+        return `${minutes}:${seconds}`
+    }
 
     decrementValue(e) {
         const [timer, value] = e.target.id.split('-')
@@ -92,30 +159,42 @@ class PomodoroClock extends Component {
         // break timer
         if (timer === 'break') {
             if (value === 'decrement' && this.state.breakLength > 1) {
-                this.setState({ breakLength: this.state.breakLength - 1 })
+                this.setState({
+                    breakLength: this.state.breakLength - 1,
+                })
             } else if (value === 'increment' && this.state.breakLength < 60) {
-                this.setState({ breakLength: this.state.breakLength + 1 })
+                this.setState({
+                    breakLength: this.state.breakLength + 1,
+                })
             }
         }
 
         // session timer
         else if (timer === 'session') {
             if (value === 'decrement' && this.state.sessionLength > 1) {
-                this.setState({ sessionLength: this.state.sessionLength - 1 })
+                this.setState({
+                    sessionLength: this.state.sessionLength - 1,
+                })
             } else if (value === 'increment' && this.state.sessionLength < 60) {
-                this.setState({ sessionLength: this.state.sessionLength + 1 })
+                this.setState({
+                    sessionLength: this.state.sessionLength + 1,
+                })
             }
+
+            this.setState({ timer: (this.state.sessionLength - 1) * 60 })
         }
     }
 
     render() {
         return (
             <div className='container'>
-                <img
-                    src={tomato}
-                    alt='Pomodoro Clock'
-                    className='icon-header'
-                />
+                <div className='container__header'>
+                    <img
+                        src={tomato}
+                        alt='Pomodoro Clock'
+                        className='icon-header'
+                    />
+                </div>
 
                 <div className='container__length'>
                     <Label label='break-label' name='Break Length'>
@@ -154,13 +233,17 @@ class PomodoroClock extends Component {
                 </div>
 
                 <div className='container__session'>
-                    <h3 id='timer-label'>{this.state.currentTimer}</h3>
+                    <h3 id='timer-label'>{this.state.timerType}</h3>
 
-                    <div id='time-left'>{this.state.timer}</div>
+                    <div id='time-left'>{this.clockify()}</div>
                 </div>
 
                 <div className='container__buttons'>
-                    <Button id='start_stop' icon={playIcon} />
+                    <Button
+                        id='start_stop'
+                        icon={playIcon}
+                        onClick={this.timerControl}
+                    />
 
                     <Button
                         id='reset'
